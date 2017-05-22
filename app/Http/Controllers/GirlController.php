3bfe6bff17d2c38de575a95083df40f0b9dfaa38
costicanu <costicanu@yahoo.com>
2017-05-22 12:30:57 +0200

    left menu + admin middleware

---
+++ app/Http/Controllers/GirlController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Girl;
+use Illuminate\Http\Request;
+
+class GirlController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Girl $girl
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Girl $girl)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Girl $girl
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Girl $girl)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \App\Girl $girl
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Girl $girl)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Girl $girl
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Girl $girl)
+    {
+        //
+    }
+}
